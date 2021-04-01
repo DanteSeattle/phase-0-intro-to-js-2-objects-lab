8ab5f5a0bbcc30791c3664eee77bb7f801c7a6bb
DanteSeattle <gabrieldantelombardi@gmail.com>
2021-04-01 14:15:15 -0700

    assignment completed

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,3 +1,32 @@
+const employee = {
+  name: "Dante",
+  streetAddress: "505"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+  const newEmployee = {...employee};
+  newEmployee[key] = value;
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+  
+  employee[key]= value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+  const newEmployee = {...employee};
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+  
+  delete employee[key];
+  return employee;
+}
+
 describe('employees', function() {
   describe('updateEmployeeWithKeyAndValue(employee, key, value)', function () {
     beforeEach(function () {
